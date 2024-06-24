ffb936418adb05909528e7465a736f4e920a1162
junghwa <killthemalllpado@gmial.com>
2024-06-24 14:17:23 +0900

    한신공영 AI 챗봇으로 GptBubble 수정, UserBubble bg 색 neutral-600으로 변경

--- src/components/bubble/GptBubble.tsx
+++ src/components/bubble/GptBubble.tsx
@@ -52,10 +52,10 @@ const GptBubble: React.FC<GptBubbleProps> = ({
       >
         <div className="justify-start items-center gap-2 inline-flex">
           <div className="w-[32px] h-[32px] rounded-full shadow-md bg-neutral-white border border-neutral-300 items-center justify-center inline-flex">
-            <Icon name="buildnow" width={24} height={24} />
+            <Icon name="hanshin" width={24} height={24} />
           </div>
           <div className="text-neutral-400 text-paragraph-l font-bold">
-            빌드나우 AI 챗봇
+            한신공영 AI 챗봇
           </div>
         </div>
         <div className="w-full pl-10 pr-2.5 py-2.5 bg-white flex-col justify-start items-start gap-4 inline-flex text-paragraph-chatBot">
