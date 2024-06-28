e9bb881516c8b7c318aed611f4a7346b07d2de37
kyle9909 <kyle9909@naver.com>
2024-06-28 18:48:55 +0900

    ks 이상함

---
+++ src/app/main/page.tsx
@@ -0,0 +1,355 @@
+"use client";
+
+import React, { useState, useEffect, Suspense } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+import TopNav from "@/components/layoutComponents/topNavs/TopNav";
+import dynamic from "next/dynamic";
+import SideBar from "@/components/layoutComponents/sideBars/SideBar";
+import { GetApiProvider } from "@/components/dropDown/GetApiContext";
+import { ActiveItemProvider } from "../../components/dropDown/ActiveItemContext";
+import { ChooseRecommendContextProvider } from "@/components/layoutComponents/loadingPages/recommend/ChooseRecommendContext";
+
+// Container 컴포넌트를 동적으로 가져오기
+const Container = dynamic(
+  () => import("@/components/layoutComponents/containers/Container"),
+  {
+    ssr: false, // 서버 사이드 렌더링 비활성화
+    suspense: true,
+  }
+);
+
+interface HomeProps {
+  searchParams: { [key: string]: string | undefined };
+}
+
+const Home: React.FC<HomeProps> = ({ searchParams }) => {
+  const pathname = usePathname();
+  const router = useRouter();
+  console.log("서치 파람스", searchParams);
+  // const searchParams = useSearchParams();
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [userLevel, setUserLevel] = useState("");
+  const [activePage, setActivePage] = useState("Container");
+  const [selectedProject, setSelectedProject] = useState<string | null>(null);
+  const [selectedMemoryId, setSelectedMemoryId] = useState<string | null>(null);
+  const [memoryIdList, setMemoryIdList] = useState<
+    Array<{ memory_id: string; last_chat_time: string; project_name: string }>
+  >([]);
+
+  //sideBar toggle
+  const handleSideBarToggle = () => {
+    setIsSidebarOpen((prevState) => !prevState);
+  };
+
+  //logout
+  const handleLogout = () => {
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("memory_id_list");
+    window.location.href = "/login";
+  };
+
+  //token 보유 유무 확인 후 리디렉션(차후 개선 필요)+토큰/memory-id-list 로컬스토리지에
+  useEffect(() => {
+    const token = localStorage.getItem("access_token");
+    if (!token) {
+      window.location.href = "/login";
+      return;
+    }
+
+    const payload = JSON.parse(atob(token.split(".")[1]));
+    setUserName(payload.username);
+
+    switch (payload.role) {
+      case "super_admin":
+        setUserLevel("최고관리자");
+        break;
+      case "admin":
+        setUserLevel("관리자");
+        break;
+      case "nomal":
+        setUserLevel("일반 사용자");
+        break;
+      default:
+        setUserLevel("알 수 없음");
+    }
+
+    // 로컬 스토리지에서 memoryIdList 가져오기
+    const storedMemoryIdList = localStorage.getItem("memory_id_list");
+    if (storedMemoryIdList) {
+      setMemoryIdList(JSON.parse(storedMemoryIdList));
+    }
+  }, []);
+
+  const handlePageChange = (page: string) => {
+    setActivePage(page);
+    updateQuery(page, selectedProject);
+  };
+  const handleProjectSelect = (projectName: string | null) => {
+    setSelectedProject(projectName);
+    if (activePage === "ProjectManagement") {
+      updateQuery("ProjectManagement", projectName);
+    }
+  };
+  const handleSelectMemory = (memoryId: string) => {
+    console.log("Home handleSelectMemory", memoryId);
+    setSelectedMemoryId(memoryId);
+  };
+
+  //브라우저 히스토리 업데이트
+  const updateQuery = (page: string, project: string | null = null) => {
+    const params = new URLSearchParams(searchParams as Record<string, string>);
+    console.log("search", searchParams);
+    console.log("params", params);
+    const currentPage = searchParams?.page || null;
+    const currentProject = searchParams?.project || null;
+
+    if (currentPage === page && currentProject === project) return;
+
+    params.set("page", page);
+    if (project !== null) {
+      params.set("project", project);
+      params.set("view", "ProjectDetail");
+    } else {
+      params.set("view", "ProjectListPage");
+      params.delete("project");
+    }
+
+    router.push(`${pathname}?${params.toString()}`);
+  };
+
+  useEffect(() => {
+    const page = searchParams.page;
+    const project = searchParams.project;
+
+    if (page && page !== activePage) {
+      setActivePage(page);
+    }
+
+    if (project && project !== selectedProject) {
+      setSelectedProject(project === "null" ? null : project);
+    }
+  }, [searchParams]);
+
+  //뒤로가기 했을 떄 selectedProject 리셋
+  useEffect(() => {
+    const handlePopState = () => {
+      const params = new URLSearchParams(window.location.search);
+      const project = params.get("project");
+
+      if (!project) {
+        setSelectedProject(null);
+      }
+    };
+
+    window.addEventListener("popstate", handlePopState);
+
+    return () => {
+      window.removeEventListener("popstate", handlePopState);
+    };
+  }, []);
+
+  return (
+    <div className="w-full flex-col items-center justify-center">
+      <GetApiProvider>
+        <ActiveItemProvider>
+          <ChooseRecommendContextProvider>
+            <TopNav
+              userLevel={userLevel}
+              userName={userName}
+              onToggleSidebar={handleSideBarToggle}
+            />
+
+            <div className="flex flex-grow">
+              <Suspense fallback={<div>Loading...</div>}>
+                {isSidebarOpen && (
+                  <SideBar
+                    isSuperAdmin={userLevel === "최고관리자"}
+                    userName={userName}
+                    userLevel={userLevel}
+                    handleLogout={handleLogout}
+                    setActivePage={handlePageChange}
+                    memoryIdList={memoryIdList}
+                    onSelectMemory={handleSelectMemory}
+                  />
+                )}
+              </Suspense>
+              <Suspense fallback={<div>Loading...</div>}>
+                <Container
+                  isSidebarOpen={isSidebarOpen}
+                  activePage={activePage}
+                  selectedProject={selectedProject}
+                  onProjectSelect={handleProjectSelect}
+                  setActivePage={handlePageChange}
+                  selectedMemoryId={selectedMemoryId}
+                />
+              </Suspense>
+            </div>
+          </ChooseRecommendContextProvider>
+        </ActiveItemProvider>
+      </GetApiProvider>
+    </div>
+  );
+};
+
+export default Home;
+
+// import React, { useState, useEffect, Suspense } from 'react';
+// import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+// import TopNav from '@/components/layoutComponents/topNavs/TopNav';
+// import Contents from '@/components/layoutComponents/containers/Contents';
+// import SideBar from '@/components/layoutComponents/sideBars/SideBar';
+// import { GetApiProvider } from '@/components/dropDown/GetApiContext';
+// import { ActiveItemProvider } from '../../components/dropDown/ActiveItemContext';
+// import { ChooseRecommendContextProvider } from '@/components/layoutComponents/loadingPages/recommend/ChooseRecommendContext';
+
+// const Home: React.FC = () => {
+//   const pathname = usePathname();
+//   const router = useRouter();
+//   const searchParams = useSearchParams();
+
+//   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+//   const [userName, setUserName] = useState('');
+//   const [userLevel, setUserLevel] = useState('');
+//   const [activePage, setActivePage] = useState('Container');
+//   const [selectedProject, setSelectedProject] = useState<string | null>(null);
+
+//   //sideBar toggle
+//   const handleSideBarToggle = () => {
+//       setIsSidebarOpen(prevState => !prevState);
+//   };
+
+//   //logout
+//   const handleLogout = () => {
+//     localStorage.removeItem('access_token');
+//     window.location.href = '/login';
+//   };
+
+//   //token 보유 유무 확인 후 리디렉션(차후 개선 필요)
+//   useEffect(() => {
+//     // 클라이언트 측에서만 실행되도록 함
+//     if (typeof window !== 'undefined') {
+//       const token = localStorage.getItem('access_token');
+//       if (!token) {
+//         window.location.href = '/login'; // 토큰이 없으면 로그인 페이지로 리디렉션
+//         return;
+//       }
+
+//       const payload = JSON.parse(atob(token.split('.')[1]));
+//       setUserName(payload.username);
+
+//       // 역할에 따른 사용자 레벨 설정
+//       switch (payload.role) {
+//         case 'super_admin':
+//           setUserLevel('최고관리자');
+//           break;
+//         case 'admin':
+//           setUserLevel('관리자');
+//           break;
+//         case 'nomal':
+//           setUserLevel('일반 사용자');
+//           break;
+//         default:
+//           setUserLevel('알 수 없음');
+//       }
+//     }
+//   }, []);
+
+//   //브라우저 히스토리 업데이트
+//   const updateQuery = (page: string, project: string | null = null) => {
+//     const params = new URLSearchParams(searchParams);
+//     const currentPage = searchParams.get('page');
+//     const currentProject = searchParams.get('project');
+
+//     if (currentPage === page && currentProject === project) return;
+
+//     params.set('page', page);
+//     if (project !== null) {
+//       params.set('project', project);
+//       params.set('view', 'ProjectDetail');
+//     } else {
+//       params.set('view', 'ProjectListPage');
+//       params.delete('project');
+//     }
+
+//     router.push(`${pathname}?${params.toString()}`);
+//   };
+
+//   const handlePageChange = (page: string) => {
+//     setActivePage(page);
+//     updateQuery(page, selectedProject);
+//   };
+
+//   const handleProjectSelect = (projectName: string | null) => {
+//     setSelectedProject(projectName);
+//     if (activePage === 'ProjectManagement') {
+//       updateQuery('ProjectManagement', projectName);
+//     }
+//   };
+
+//   useEffect(() => {
+//     const page = searchParams.get('page');
+//     const project = searchParams.get('project');
+
+//     if (page && page !== activePage) {
+//       setActivePage(page);
+//     }
+
+//     if (project && project !== selectedProject) {
+//       setSelectedProject(project === 'null' ? null : project);
+//     }
+//   }, [searchParams]);
+
+//   //뒤로가기 했을 떄 selectedProject 리셋
+//   useEffect(() => {
+//     const handlePopState = () => {
+//       const params = new URLSearchParams(window.location.search);
+//       const project = params.get('project');
+
+//       if (!project) {
+//         setSelectedProject(null);
+//       }
+//     };
+
+//     window.addEventListener('popstate', handlePopState);
+
+//     return () => {
+//       window.removeEventListener('popstate', handlePopState);
+//     };
+//   }, []);
+
+//   return (
+//     <div className="w-full flex-col items-center justify-center">
+//       <GetApiProvider>
+//         <ActiveItemProvider>
+//           <ChooseRecommendContextProvider>
+
+//             <TopNav userLevel={userLevel} userName={userName} onToggleSidebar={handleSideBarToggle}/>
+
+//             <div className='flex flex-grow'>
+//               {isSidebarOpen && <SideBar isSuperAdmin={userLevel === '최고관리자'}
+//                                          userName={userName}
+//                                          userLevel={userLevel}
+//                                          handleLogout={handleLogout}
+//                                          setActivePage={handlePageChange}/>}
+//                 <Suspense fallback={<div>Loading...</div>}>
+//                 <Contents
+//                   isSidebarOpen={isSidebarOpen}
+//                   activePage={activePage}
+//                   selectedProject={selectedProject}
+//                   onProjectSelect={handleProjectSelect}
+//                   setActivePage={handlePageChange} />
+//                 </Suspense>
+//             </div>
+
+//           </ChooseRecommendContextProvider>
+//         </ActiveItemProvider>
+//       </GetApiProvider>
+//     </div>
+//   );
+
+// }
+
+// export default Home;
---
+++ src/components/layoutComponents/containers/ChatBot.tsx
@@ -0,0 +1,189 @@
+"use client";
+
+import React, { useState, useEffect, useRef } from "react";
+import { useRouter, useSearchParams, usePathname } from "next/navigation";
+import UserBubble from "../../chatbotComponents/bubbles/UserBubble";
+import GptBubble from "../../chatbotComponents/bubbles/GptBubble";
+import InputNomal from "../../inputs/InputNomal";
+
+import TypingIndicator from "../../chatbotComponents/bubbles/TypingIndicator";
+import SystemUpdate from "../loadingPages/SystemUpdate";
+import NoSelectedProject from "../loadingPages/NoSelectedProject";
+import RecommendedValue from "../loadingPages/recommend/RecommendedValue";
+
+import { useActiveItemContext } from "../../dropDown/ActiveItemContext";
+import { useGetApiContext } from "../../dropDown/GetApiContext";
+
+interface ChatBotProps {
+  isSidebarOpen: boolean;
+  selectedMemoryId: string | null;
+  searchParams: { [key: string]: string | undefined };
+}
+
+interface Message {
+  type: string;
+  text: string;
+  sources?: any[];
+}
+
+const ChatBot: React.FC<ChatBotProps> = ({
+  isSidebarOpen,
+  selectedMemoryId,
+  searchParams,
+}) => {
+  const { selectedProjectForChat } = useActiveItemContext();
+  const { getApi } = useGetApiContext();
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [typingComplete, setTypingComplete] = useState(true);
+  const [messagesFetched, setMessagesFetched] = useState(false);
+
+  const router = useRouter();
+  // const searchParams = useSearchParams();
+  const pathname = usePathname();
+
+  const addUserMessage = (message: string) => {
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { type: "human", text: message },
+    ]);
+  };
+  const addGptMessage = (message: string, sources: any[]) => {
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { type: "ai", text: message, sources },
+    ]);
+  };
+
+  //sideBar에서 선택한 memoryId log 불러와서 bubble로 렌더링
+  const fetchMessagesForMemoryId = async (memoryId: string) => {
+    console.log("Fetching messages for memoryId:", memoryId);
+    setMessagesFetched(false);
+    try {
+      // API 호출하여 해당 memoryId의 대화 내용 불러오기
+      const response = await fetch(
+        `https://port-0-hanshin-chatbot-be-1272llwsz1ihz.sel5.cloudtype.app/chat-history/${memoryId}`
+      );
+      const data = await response.json();
+      console.log("API response data:", data);
+      const formattedMessages = data.map((message: string) => {
+        const isHuman = message.startsWith("HumanMessage:");
+        return {
+          type: isHuman ? "human" : "ai",
+          text: message
+            .replace("HumanMessage: ", "")
+            .replace("AIMessage: ", ""),
+        };
+      });
+      setMessages(formattedMessages);
+      setMessagesFetched(true);
+    } catch (error) {
+      console.error("Error fetching messages:", error);
+      setMessages([]); // 에러 발생 시 빈 배열로 초기화
+      setMessagesFetched(false);
+    }
+  };
+
+  useEffect(() => {
+    console.log("SelectedMemoryId changed:", selectedMemoryId);
+    if (selectedMemoryId) {
+      fetchMessagesForMemoryId(selectedMemoryId);
+    }
+  }, [selectedMemoryId]);
+
+  //신규 메세지에 포커스 -> 스크롤 아래로 내리기 함수
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+  const scrollToBottom = () => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  // URL 쿼리 업데이트 함수
+  const updateQuery = (project: string | null, memoryId: string | null) => {
+    const params = new URLSearchParams(searchParams as Record<string, string>);
+    console.log("search", searchParams);
+    console.log("params", params);
+    const currentProject = searchParams?.project || null;
+    const currentMemoryId = searchParams?.memoryId || null;
+
+    if (currentProject === project && currentMemoryId === memoryId) return;
+
+    if (project !== null) {
+      params.set("project", project);
+    } else {
+      params.delete("project");
+    }
+
+    if (memoryId !== null) {
+      params.set("memoryId", memoryId);
+    } else {
+      params.delete("memoryId");
+    }
+
+    router.push(`${pathname}?${params.toString()}`);
+  };
+
+  // selectedProjectForChat 또는 selectedMemoryId가 변경될 때마다 URL 업데이트
+  useEffect(() => {
+    updateQuery(selectedProjectForChat, selectedMemoryId);
+  }, [selectedProjectForChat, selectedMemoryId]);
+
+  return (
+    <div className="flex-grow flex flex-col h-full items-center mb-[80px]">
+      <div className="w-full flex-grow flex-col overflow-y-auto h-full">
+        {getApi === false ? (
+          <SystemUpdate />
+        ) : selectedProjectForChat === null ? (
+          <div className="w-full h-full flex justify-center items-center">
+            <NoSelectedProject />
+          </div>
+        ) : messages.length === 0 ? (
+          <div className="w-full h-full flex justify-center items-center">
+            <RecommendedValue />
+          </div>
+        ) : (
+          <>
+            {messages.map((message, index) =>
+              message.type === "human" ? (
+                <UserBubble key={index} userText={message.text} />
+              ) : (
+                <GptBubble
+                  key={index}
+                  gptText={message.text}
+                  sources={message.sources || []}
+                  setTypingComplete={setTypingComplete}
+                  messagesFetched={messagesFetched}
+                />
+              )
+            )}
+            {loading && <TypingIndicator />}{" "}
+            {/* 로딩 중이면 typingIndicator 렌더링 */}
+          </>
+        )}
+        <div ref={messagesEndRef} />
+      </div>
+      <div
+        className={`fixed bottom-0
+                                ${
+                                  isSidebarOpen
+                                    ? "pl-[336px] w-full right-0"
+                                    : "max-w-[768px] w-full"
+                                }`}
+      >
+        <InputNomal
+          addUserMessage={addUserMessage}
+          addGptMessage={(message, sources) => addGptMessage(message, sources)}
+          setLoading={setLoading}
+          loading={loading}
+          typingComplete={typingComplete}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ChatBot;
